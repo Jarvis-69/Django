664bdb44657b500dd300e206b0229ce9b505d37e
Jarvis <joris.salvador@icloud.com>
2024-12-03 14:15:43 +0100

    import trad

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',  # Notre application blog
+    'rosetta',  # Application pour la traduction
 ]
  
 # Middleware
--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -5,6 +5,8 @@ from django.urls import path
 from django.contrib import admin
 from blog import views
 from django.contrib.auth import views as auth_views
+from rosetta import urls as rosetta_urls
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,4 +16,5 @@ urlpatterns = [
     path('delete/<int:pk>/', views.delete_post, name='delete_post'),
     path('login/', auth_views.LoginView.as_view(next_page='post_list'), name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('rosetta/', include(rosetta_urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
