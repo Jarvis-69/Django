7b790b9267b47942a0e5fcafbef6b6e67d5ce5b8
Jarvis <joris.salvador@icloud.com>
2024-12-03 15:17:30 +0100

    ajout filtre

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,30 +1,20 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from django.urls import reverse
+# from django.urls import reverse
 from django.utils.text import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
-    slug = models.SlugField(unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        verbose_name_plural = 'categories'
-        ordering = ('name',)
     
     def __str__(self):
         return self.name
-
-    def get_absolute_url(self):
-        return reverse('blog:category_detail', args=[self.slug])
 
 class Post(models.Model):    
     title = models.CharField(max_length=200)    
     slug = models.SlugField(unique=True, blank=True)    
     content = models.TextField()     
-    updated_at = models.DateTimeField(auto_now=True)
+    # updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='posts')    
